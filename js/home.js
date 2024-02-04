6c033ceff89268380e038cb34fce0b618a439bb1
Raunak7t <raunak7t@gamil.com>
2024-02-05 04:05:43 +0530

    added home page

--- js/home.js
+++ js/home.js
@@ -1,8 +1,25 @@
 // firebase
 
-firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-  } else {
-    window.location.assign("../index.html");
-  }
+// firebase.auth().onAuthStateChanged((user) => {
+//   if (user) {
+//   } else {
+//     window.location.assign("../index.html");
+//   }
+// });
+
+const settingsMenu = document.querySelector(".setting-menu");
+const storiesDiv = document.querySelector(".story-gallery");
+const categoryBar = document.querySelector(".category-bar");
+
+function settingsMenuToggle() {
+  settingsMenu.classList.toggle("settings-menu-height");
+}
+
+storiesDiv.addEventListener("wheel", (evt) => {
+  evt.preventDefault();
+  storiesDiv.scrollLeft += evt.deltaY;
+});
+categoryBar.addEventListener("wheel", (evt) => {
+  evt.preventDefault();
+  categoryBar.scrollLeft += evt.deltaY;
 });
