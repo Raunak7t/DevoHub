a7af16088f5a187dde5d28e4165c00d917983390
Raunak7t <raunak7t@gamil.com>
2024-02-06 01:56:18 +0530

    modified nav

--- js/home.js
+++ js/home.js
@@ -7,14 +7,6 @@
 //   }
 // });
 
-const settingsMenu = document.querySelector(".setting-menu");
-const storiesDiv = document.querySelector(".story-gallery");
-const categoryBar = document.querySelector(".category-bar");
-
-function settingsMenuToggle() {
-  settingsMenu.classList.toggle("settings-menu-height");
-}
-
 storiesDiv.addEventListener("wheel", (evt) => {
   evt.preventDefault();
   storiesDiv.scrollLeft += evt.deltaY;
---
+++ js/nav.js
@@ -0,0 +1,7 @@
+const settingsMenu = document.querySelector(".setting-menu");
+const storiesDiv = document.querySelector(".story-gallery");
+const categoryBar = document.querySelector(".category-bar");
+
+function settingsMenuToggle() {
+  settingsMenu.classList.toggle("settings-menu-height");
+}
