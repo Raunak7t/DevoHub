7d0a7fc8854ad1b8a4af256d5088fc7cd27d1471
Raunak Tiwari <113747174+Raunak7t@users.noreply.github.com>
2024-03-05 15:17:10 +0530

    login path

--- js/log-in.js
+++ js/log-in.js
@@ -5,10 +5,11 @@ let loginForm = document.querySelector(".login-form");
 loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
   firebase
-  .auth()
-  .signInWithEmailAndPassword(logEmail.value, logPassword.value)
-  .then((userCredential) => {
-      window.location.href = `${window.location.href}pages/home.html`;
+    .auth()
+    .signInWithEmailAndPassword(logEmail.value, logPassword.value)
+    .then((userCredential) => {
+      // window.location.href = `${window.location.href}pages/home.html`;
+      window.location = "./pages/home.html";
     })
     .catch((error) => {
       console.log(error);
